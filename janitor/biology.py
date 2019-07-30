c249239f0342233f283d8ab8ec1068a0c0900dc4
Eric Ma <ericmajinglong@gmail.com>
2019-07-29 22:27:22 -0400

    first sketch on docstrings

--- janitor/biology.py
+++ janitor/biology.py
@@ -2,7 +2,10 @@
 Biology and bioinformatics-oriented data cleaning functions.
 """
 
+from typing import Union
+
 import pandas as pd
+
 import pandas_flavor as pf
 
 from .utils import deprecated_alias, import_message
@@ -79,3 +82,37 @@ def join_fasta(
     seq_col = [seqrecords[i] for i in df[id_col]]
     df[column_name] = seq_col
     return df
+
+
+@pf.register_dataframe_method
+def sequence_features(
+    df: pd.DataFrame,
+    column_name: str,
+    kind: str,
+    featurization_type: Union[str, int],
+    pad_character="-",
+    sequence_length="most_common_only",
+):
+    """
+    Return sequence features as a new pandas DataFrame.
+
+
+    :param df: The pandas DataFrame
+        containing a column of sequences (as strings)
+    :param column_name: The column that houses the sequences.
+    :param kind: One of (``protein``, ``dna``)
+    :param featurization_type: One of
+        (``one-hot``, ``pka``, ``mw``, and ``t``)
+    :param pad_character: The character to use to pad the sequence.
+    :param sequence_length: One of
+        (``most_common_only``, ``most_common_max``, ``longest``, or a number).
+        ``most_common_only`` keeps only sequences of the most common length.
+        ``most_common_max`` keeps sequences up to the most common length,
+        and right-pads any sequences of smaller length
+        with a dash character.
+        ``longest`` keeps all sequences,
+        and right-pads any sequence of smaller length
+        with a dash character.
+        Putting in an integer keeps only sequences of that given length.
+    """
+    pass
